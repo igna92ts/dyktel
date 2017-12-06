f025a7ad76a4bc640a88a433bfc44c5c7620f12d
Ignacio Torres <igna_torres92@hotmail.com>
2017-12-06 19:15:45 -0300

    So much stuff

--- src/form_sender.js
+++ src/form_sender.js
@@ -1,14 +1,24 @@
 const puppeteer = require('puppeteer'),
-  config = require('../steps.config');
+  stepsConfig = require('../steps.config');
 
-const executeSite = async (page, step, index) => {
+const batchSize = 10;
+let progress = 0;
+
+const executeSite = async (page, step) => {
   if (step.site) {
     await page.goto(step.site, {waitUntil: 'networkidle2'});
   }
   for (let i = 0; i < step.inputs.length; i ++) {
     const input = step.inputs[i];
     await page.waitForSelector(input.selector);
-    await page[input.action](input.selector, input.value);
+    if (input.value) {
+      await page[input.action](input.selector, input.value);
+    } else {
+      await page[input.action](input.selector);
+    }
+    if (input.delay) {
+      await page.waitFor(input.delay);
+    }
   }
   // await page.screenshot({ path: `example${index}.png` })
   await page.waitFor(step.finisher.selector);
@@ -16,13 +26,29 @@ const executeSite = async (page, step, index) => {
   await page.waitFor(step.finisher.waitFor);
 }
 
-exports.executeSteps = async () => {
-  const steps = config.steps;
-  const browser = await puppeteer.launch({headless: false});
-
-  const page = await browser.newPage();
-  for (let i = 0; i < steps.length; i++) {
-    await executeSite(page, steps[i], i);
+const executeBatch = async (browser, data) => {
+  const dataHead = data.slice(0, batchSize);
+  const dataRest = data.slice(batchSize);
+  let promises = dataHead.map(async csvRow => {
+    const steps = stepsConfig(csvRow).steps;
+    const page = await browser.newPage();
+    for (let i = 0; i < steps.length; i++) {
+      await executeSite(page, steps[i]);
+    }
+    await page.close();
+  });
+  if (dataRest.length > 0) {
+    const recur = await executeBatch(browser, dataRest)
+    promises = promises.concat(recur);
   }
+  return promises;
+}
+
+exports.executeSteps = async data => {
+  const browser = await puppeteer.launch({headless: true});
+  progress = 0;
+  const result = await executeBatch(browser, data);
+  await Promise.all(result);
+  console.log('DONE');
   await browser.close();
 }
--- steps.config.js
+++ steps.config.js
@@ -1,61 +1,81 @@
-module.exports = {
-  steps: [
-    {
-      site: 'https://docs.google.com/forms/d/e/1FAIpQLSdg3p7yvQNT9QDTpZBkyGACQfnjsud1LQB3Iz0aJ6AbWSbKoQ/viewform',
-      inputs: [
-        {
-          selector: 'input[type="email"][name="identifier"]',
-          action: 'type',
-          value: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+module.exports = csvRow => {
+  return {
+    steps: [
+      {
+        site: 'https://docs.google.com/forms/d/e/1FAIpQLSdg3p7yvQNT9QDTpZBkyGACQfnjsud1LQB3Iz0aJ6AbWSbKoQ/viewform',
+        inputs: [
+          {
+            selector: 'input[type="email"][name="identifier"]',
+            action: 'type',
+            value: 'aaaaaaaaaaaaaa'
+          }
+        ],
+        finisher: {
+          selector: 'div[role="button"][jsname="tJiF1e"]',
+          action: 'click',
+          waitFor: 1000
         }
-      ],
-      finisher: {
-        selector: 'div[role="button"][jsname="tJiF1e"]',
-        action: 'click',
-        waitFor: 1000
-      }
-    },
-    {
-      inputs: [
-        {
-          selector: 'input[name="password"]',
-          action: 'type',
-          value: 'aaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      },
+      {
+        inputs: [
+          {
+            selector: 'input[name="password"]',
+            action: 'type',
+            value: 'aaaaaaaaaaaaaaaaaaa'
+          }
+        ],
+        finisher: {
+          selector: 'div[role="button"][jsname="tJiF1e"]',
+          action: 'click',
+          waitFor: 'input[name="entry.45060985"]'
         }
-      ],
-      finisher: {
-        selector: 'div[role="button"][jsname="tJiF1e"]',
-        action: 'click',
-        waitFor: 'input[name="entry.45060985"]'
-      }
-    },
-    {
-      inputs: [
-        {
-          selector: 'input[name="entry.45060985"]',
-          action: 'type',
-          value: 'input de la seccion 1'
+      },
+      {
+        inputs: [
+          {
+            selector: 'input[name="entry.45060985"]',
+            action: 'type',
+            value: csvRow.input1
+          }
+        ],
+        finisher: {
+          selector: 'div[role="button"][jsname="OCpkoe"]',
+          action: 'click',
+          waitFor: 1000
         }
-      ],
-      finisher: {
-        selector: 'div[role="button"][jsname="OCpkoe"]',
-        action: 'click',
-        waitFor: 1000
-      }
-    },
-    {
-      inputs: [
-        {
-          selector: 'input[name="entry.45060985"]',
-          action: 'type',
-          value: 'input de la seccion 1'
+      },
+      {
+        inputs: [
+          {
+            selector: 'div.quantumWizMenuPaperselectOptionList[jsname="d9BH4c"]',
+            action: 'click',
+            delay: 1000
+          },
+          {
+            selector: `div[role="option"][jsname="wQNmvb"][data-value="${csvRow.select}"]`,
+            action: 'click',
+            delay: 1000
+          },
+          {
+            selector: 'input[name="entry.1332089574"]',
+            action: 'type',
+            value: csvRow.input2
+          },
+          {
+            selector: `div[role="checkbox"][aria-label="${csvRow.checkbox}"]`,
+            action: 'click'
+          },
+          {
+            selector: `div[role="radio"][aria-label="${csvRow.radio}"]`,
+            action: 'click'
+          }
+        ],
+        finisher: {
+          selector: 'div[role="button"][jsname="M2UYVd"]',
+          action: 'click',
+          waitFor: 2000
         }
-      ],
-      finisher: {
-        selector: 'div[role="button"][jsname="OCpkoe"]',
-        action: 'click',
-        waitFor: 1000
       }
-    }
-  ]
+    ]
+  }
 }
